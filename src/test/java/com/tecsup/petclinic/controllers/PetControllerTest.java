b01c12d5ec5cf1c631b8729901c6e9f66ab3a26e
Your Name <you@example.com>
2023-11-24 11:40:12 -0500

    test

--- src/test/java/com/tecsup/petclinic/controllers/PetControllerTest.java
+++ src/test/java/com/tecsup/petclinic/controllers/PetControllerTest.java
@@ -37,41 +37,7 @@ class PetControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void testGetPets() throws  Exception{
-        int ID_FIRST_RECORD = 1;
-         this.mockMvc.perform(get("/pets"))
-                 .andExpect(status().isOk())
-                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                 .andExpect(jsonPath("$[0].id").value(ID_FIRST_RECORD));
-    }
 
-    @Test
-    public void testFindPetOK() throws Exception{
-        int ID = 1;
-        String NAME_PET= "Leo";
-        int TYPE_ID= 1;
-        int OWNER_ID = 1;
-        Date DATE = new SimpleDateFormat("yyyy-MM-dd").parse("2023-11-01");
-        String DATE_STRING = new SimpleDateFormat("yyyy-MM-dd").format(DATE);
-
-
-        this.mockMvc.perform(get("/pets/1"))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect( jsonPath("$.id").value(ID))
-                .andExpect( jsonPath("$.name").value(NAME_PET))
-                .andExpect( jsonPath("$.typeId").value(TYPE_ID))
-                .andExpect( jsonPath("$.ownerId").value(OWNER_ID))
-                .andExpect( jsonPath("$.birth_date").value(DATE_STRING));
-
-    }
-
-    @Test
-    public void testFindPetKO() throws Exception {
-        this.mockMvc.perform(get("/pets/666"))
-                .andExpect(status().isNotFound());
-    }
 
     @Test
     public void testCreatePet() throws Exception {
@@ -96,27 +62,6 @@ class PetControllerTest {
     }
 
 
-    @Test
-    public void testDeletePet() throws Exception{
-        String NAME_PET = "BeethovenY";
-        int TYPE_ID = 1;
-        int OWNER_ID = 1;
-        String DATE_REF = "2021-11-16";
-        java.sql.Date DATE =  java.sql.Date.valueOf(DATE_REF);
-        PetDTO newPet = new PetDTO(NAME_PET,TYPE_ID,OWNER_ID, DATE);
-
-        ResultActions mvcActions = mockMvc.perform(post("/pets")
-                .content(om.writeValueAsString(newPet))
-                .header(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isCreated());
-
-        String response = mvcActions.andReturn().getResponse().getContentAsString();
-        Integer id = JsonPath.parse(response).read("$.id");
-        mockMvc.perform(delete("/pets/"+id))
-             //   .andDo(print())
-                .andExpect(status().isOk());
-    }
 
 
 }
